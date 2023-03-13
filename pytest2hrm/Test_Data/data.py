546664a4508d93f2cee166b7b800263342f84d27
Abdulsalam26 <117555399+Abdulsalam26@users.noreply.github.com>
2023-03-13 08:11:04 +0530

    Add files via upload

---
+++ pytest2hrm/Test_Data/data.py
@@ -0,0 +1,145 @@
+# data.py - Keep all your Data in this file
+class Hrm_Data:
+    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'
+
+    username = 'Admin'
+
+    password = 'admin123'
+    
+    searchbox_upper = 'Admin'
+    
+    searchbox_lower = 'admin'
+    
+    searchbox_pimU = 'PIM'
+    
+    searchbox_pimL = 'pim'
+        
+    leaveup = 'LEAVE'
+    
+    leavelow = 'leave'  
+    
+    timeup = 'TIME'
+    
+    timelow = 'time' 
+    
+    recruitmentup = 'RECRUITMENT'
+    
+    recruitmentlow = 'recruitment' 
+    
+    myinfoup = 'MY INFO'
+
+    myinfolow = 'my info'
+    
+    performanceup = 'PERFORMANCE'
+    
+    performancelow ='performance'
+       
+    dashboardup = 'DASHBOARD'
+    
+    dashboardlow = 'dashboard'
+    
+    directoryup = 'DIRECTORY'
+    
+    directorylow = 'directory'
+    
+    maintanenceup = 'MAINTENANCE'
+    
+    a_password = 'admin123'
+    
+    maintanancelow = 'maintenance'
+    
+    buzzup = 'BUZZ' 
+    
+    buzzlow = 'buzz'   
+    
+    entry_name ='Admin'
+    
+    name = 'Abdulsalam'
+    
+    lastname = 'M'
+    
+    login_username = 'Ashik'
+    
+    login_password = 'Abdu@123'
+    
+    confirm_password = 'Abdu@123' 
+    
+    employeename = 'Abdulsalam'
+    
+    otherid = '225588'
+    
+    license_number = '786786'
+    
+    license_expire_date = '2023-09-22'
+    
+    ssn_number = '2587410'
+    
+    sin_number = '9854136'
+    
+    date_of_birth = '1996-09-22'
+    
+    militry_serives = 'No'
+    
+    street = 'Kumar nagar'
+    
+    street_name = 'west street'
+    
+    city_name = 'Tirupur'
+    
+    state_name = 'Tamil Nadu'
+    
+    zip_code = '641603'
+    
+    home_no = '753014'
+    
+    mobile_no = '8667813697'
+    
+    work_name = '1'
+    
+    work_mail = 'abdu@gmail.com'
+    
+    other_mail = 'abdu123@gmail.com'
+    
+    Name = 'Nisha'
+    
+    Relationship ='wife'
+    
+    Home_Telephone = '458956'
+    
+    Mobile_number = '9787362733'
+    
+    work_Telephone = '0421-255863'
+    
+    Relation_name = 'Appas'
+    
+    Relation_dob = '2022-04-15'
+    
+    start_date = '2023-03-12'
+    
+    end_date = '2024-09-12'
+    
+    end_date = '2024-09-12'
+    
+    exit_DOB = '2022-03-12'
+    
+    Text = 'I have a learnt lot from this company...Thank you all'
+    
+    salarycomponent = '25000'
+    
+    salary_amount = '25000'
+    
+    account_no = '50100213868'
+    
+    Routing_no = '13698744'
+    
+    deposit_amount = '25000'
+   
+    value_Exemptions = '1'
+    
+    per_Exemptions = '2'
+    
+
+
+    
+   
+    
